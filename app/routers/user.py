3277f94921e33d1315aac64527d0e1bab0f49a93
Feo255 <dyngafiodor@gmail.com>
2024-12-06 15:52:55 +0300

    Add files via upload

---
+++ app/routers/user.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+
+
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+@router.get("/")
+async def get_all_users():
+    pass
+
+@router.get("/user_id")
+async def get_user():
+    pass
+
+@router.post("/create")
+async def create_user():
+    pass
+
+
+@router.put("/update")
+async def update_user():
+    pass
+
+
+@router.delete("/delete")
+async def delete_user():
+    pass
